9321a36c1c02528e7184dfff7b9ad7f719abf81d
Artem Pryzhkov <Artem_Pryzhkov@epam.com>
2016-10-06 14:37:25 +0300

    add appContext1.xml

---
+++ src/main/java/ua/rd/pizza/services/SomeService.java
@@ -0,0 +1,7 @@
+package ua.rd.pizza.services;
+
+/**
+ * Created by Artem_Pryzhkov on 10/6/2016.
+ */
+public interface SomeService {
+}
---
+++ src/main/java/ua/rd/pizza/services/Test1SomeService.java
@@ -0,0 +1,7 @@
+package ua.rd.pizza.services;
+
+/**
+ * Created by Artem_Pryzhkov on 10/6/2016.
+ */
+public class Test1SomeService {
+}
